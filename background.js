99d9605354f67fe32ae59661e6a34c2048e6f5cb
Jan Cholasta <jan@cholasta.net>
2017-12-06 19:28:06 +0100

    Follow Airbnb code style

--- background.js
+++ background.js
@@ -1,41 +1,45 @@
-(async() => {
-	'use strict';
+(async () => {
+  'use strict';
 
-	const {windows, tabs} = browser;
-	let openerTabId = tabs.TAB_ID_NONE;
+  const { windows, tabs } = browser;
+  let openerTabId = tabs.TAB_ID_NONE;
 
-	tabs.onActivated.addListener(activeInfo => {
-		openerTabId = activeInfo.tabId;
-	});
+  tabs.onActivated.addListener((activeInfo) => {
+    openerTabId = activeInfo.tabId;
+  });
 
-	tabs.onCreated.addListener(async tab => {
-		if (tab.id === tabs.TAB_ID_NONE ||
-			openerTabId === tabs.TAB_ID_NONE ||
-			tab.discarded)
-			return;
+  tabs.onCreated.addListener(async (tab) => {
+    if (tab.id === tabs.TAB_ID_NONE
+      || openerTabId === tabs.TAB_ID_NONE
+      || tab.discarded) {
+      return;
+    }
 
-		let openerTab = await tabs.get(openerTabId);
-		let currentWindow = await windows.getCurrent({
-			populate: true
-		});
+    const openerTab = await tabs.get(openerTabId);
+    const currentWindow = await windows.getCurrent({
+      populate: true,
+    });
 
-		if (openerTab.windowId !== currentWindow.id)
-			return;
+    if (openerTab.windowId !== currentWindow.id) {
+      return;
+    }
 
-		let index = openerTab.index + 1;
+    let index = openerTab.index + 1;
 
-		while (currentWindow.tabs[index].pinned)
-			index++;
-		while (currentWindow.tabs[index].openerTabId === openerTabId)
-			index++;
+    while (currentWindow.tabs[index].pinned) {
+      index += 1;
+    }
+    while (currentWindow.tabs[index].openerTabId === openerTabId) {
+      index += 1;
+    }
 
-		await tabs.move(tab.id, {
-			index: index
-		});
-	});
+    await tabs.move(tab.id, {
+      index,
+    });
+  });
 
-	[{id: openerTabId}] = await tabs.query({
-		active: true,
-		currentWindow: true
-	});
+  [{ id: openerTabId }] = await tabs.query({
+    active: true,
+    currentWindow: true,
+  });
 })();
